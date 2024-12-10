c41b259182fe3b0962280326afb42f240b9033f4
wlsdnjs1120 <wlsdnjs1120@naver.com>
2024-12-10 12:06:32 +0900

    controller, h2 setting test

--- src/main/java/com/example/sbb/MainController.java
+++ src/main/java/com/example/sbb/MainController.java
@@ -1,17 +1,30 @@
 package com.example.sbb;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 public class MainController {
     @GetMapping("/")
-    public void home(){
+    @ResponseBody
+    public int home(){
         System.out.println("home");
+        return 22;
     }
 
 
     @GetMapping("/about")
-    public void about(){
+    @ResponseBody
+    public String about(){
         System.out.println("about");
+        return "안녕하세요.";
+
+    }
+
+    public void contact(){
+        System.out.println("contact");
     }
 
 }
